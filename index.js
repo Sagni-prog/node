dc6fbf14dc8ac9975adba0558c85b1f6b7dc1ff9
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-11 23:49:00 +0300

    ..

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const url = require('url');
 
 try {
 
-//   const text = fs.readFileSync('./files/input.txt','utf-8');
+const templates = (template,content) => {
+
+   let HtmlTemplete = template.replace(/{{title}}/g,content.title);
+   HtmlTemplete = HtmlTemplete.replace(/{{body}}/g,content.body);
+   HtmlTemplete = HtmlTemplete.replace(/{{photo}}/g,content.photo_url);
+   
+   return HtmlTemplete;
+}
+
+	const data =fs.readFileSync(`${__dirname}/files/data.json`,'utf-8');
+	
+	const temp = fs.readFileSync(`${__dirname}/temp.html`,'utf-8');
+	const card = fs.readFileSync(`${__dirname}/index.html`,'utf-8');
+	
+	const postData = JSON.parse(data);
 	
 	const server = http.createServer((req,res) => {
 	    const path = req.url;
@@ -12,23 +26,31 @@ try {
 	    switch(path){
 	    case '/':
 			res.writeHead(200,{
-			  'Content-type': 'text/html'
+			'Content-type' : 'text/html'
 			});
-			res.end('this is root path');
+			
+			const postHtml = postData.map(el => templates(card,el)).join('');
+			// console.log(postHtml)
+			
+			res.end(postHtml);
+			
+			
+			
+			
 		case '/posts': 
 			res.writeHead(200,{
-				'Content-type': 'text/html'
+				'Content-type': 'application/json'
 			  });
-	        res.end('this is the posts');
+			//   res.end(data);
 	    default: 
 			res.writeHead(404,{
 				'Content-type': 'text/html'
 			  });
-	        res.end('<h1>404 page not found</h1>')
-	    } 
+	        // res.end('<h1>404 page not found</h1>')
+	    }
 	});
 	
-	server.listen(400,() => {
+	server.listen(2000,() => {
 		console.log('server running on port 400');
 	});
 	
