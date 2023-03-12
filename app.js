671ff87e4faba631e8f7876529e56d999c738197
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-12 11:03:04 +0300

    event loop

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ fs.readFile('./files/input.txt','utf-8',() => {
   setTimeout(() => console.log("Timer 2 from callback"),0);
   setTimeout(() => console.log("Timer 3 from callback"),2000);
   setImmediate(() => console.log("Immediate 2 from callback"));
+  
+  process.nextTick(() => console.log("process.nettTick"));
 });
 
-console.log("synchroneous");
+console.log("syn chroneous");
 
 
 
