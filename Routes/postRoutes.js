40e81f42438c0404838b442eabecd66ba223a0f2
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-14 09:36:58 +0300

    validator using middleware

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -12,7 +12,7 @@ router.param('id',PostController.checkId);
 // middleware that validates fields
 const checkBody = (req,res,next) => {
    if(req.method === 'POST'){
-       if(!req.body.title || !req.body.body){
+       if(!req.body.title || !req.body.body || req.body.photo_url){
            return res.status(400).json({
                status : 'fail',
                message: 'fields cant be null'
