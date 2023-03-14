6712ab6e4815914abc2ea9241f0733b6c2830de4
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-14 10:21:44 +0300

    evironment variables

--- post.js
+++ post.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const fs = require('fs');
+const dotenv = require('dotenv');
+
 const morgan = require('morgan');
 const postRouter = require('./Routes/postRoutes');
 const userRouter = require('./Routes/userRoutes');
 
+dotenv.config({path: './config.env'});
+
+
+
 const app = new express();
 app.use(express.json());
-app.use(morgan('dev'));
+
+if(process.env.NODE_ENV === 'development'){
+   app.use(morgan('dev'));
+}
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next) => {
@@ -16,9 +25,11 @@ app.use((req,res,next) => {
 
  app.use('/api/v1/posts',postRouter);
  app.use('/api/v1/users',userRouter);
+ 
+//  console.log(process.env)
+//  console.log(process.env.password)
 
-
-const port = 4000;
+const port = process.env.port;
 app.listen(port,() => {
      console.log("app running on port 4000");
 });
