524c45a462fff8b7dabdcf6b85b79aa50d82ed94
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-16 23:07:37 +0300

    more on mongoose

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,3 +1,4 @@
+const { findByIdAndUpdate } = require('./../Models/Post');
 const Post = require('./../Models/Post'); 
 
 
@@ -64,48 +65,27 @@ const Post = require('./../Models/Post');
    }
     
  }
- exports.getPostById = (req,res) => {
-    const id = req.params.id * 1;
-    
-      if(!post){
-         return res.status(404).json({
-              status: "fail", 
-              message: "not found", 
-           }); 
-      }
-    res.status(200).json({
-       status: "success", 
-       data: {
-          post
-       }
-       
-    }); 
-  }
   
-   exports.updatePost = (req,res) => {
-
-    const id = req.params.id * 1;
-    const newPost = req.body;
-         const post = posts.find(el => el.post_id === id);
-         const firstSlice = posts.slice(0,id - 1);
-         const secondSlice = posts.slice(id,posts.length);
-         
-         const updatedPost = Object.assign({post_id: id, },  newPost);
-         
-         const newPosts = [
-              firstSlice,
-              updatedPost,
-              secondSlice
-         ];
-         
-         fs.writeFile(`${__dirname}/learn/data/posts.json`,JSON.stringify(newPosts),err => {
-            res.status(201).json({
-            status: "success", 
-            data: {
-               newPosts
-            }
-            });
-        });
-        
-    }
-  
+ 
+   exports.update = async(req,res) => {
+   
+   try {
+      const posts = findByIdAndUpdate(req.body.id,req.body,{
+         new: true,
+         runValidators: true
+   });
+   
+   res.status(200).json({
+         status: "success",
+         data: {
+               posts
+         }
+   });
+   } catch (error) {
+       res.status(400).json({
+            status: "fail",
+            message: error
+       });
+   }     
+}
+   
