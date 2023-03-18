20224052dffa570e26067002bcafea73c2ce51fc
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-18 10:40:12 +0300

    global error handling in express

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const postRouter = require('./Routes/postRoutes');
 const userRouter = require('./Routes/userRoutes');
-const PostController = require('./Controllers/PostController');
+// const PostController = require('./Controllers/PostController');
 
 dotenv.config({path: './.env'});
 
@@ -32,11 +32,34 @@ app.get('/',(req,res) => {
 
    
 app.all('*',(req,res,next) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Cant find ${req.originalUrl} on this server`
+    // res.status(404).json({
+    //     status: "fail",
+    //     message: `Cant find ${req.originalUrl} on this server`
+    // });
+    
+    // next();
+    
+    const err = new Error(`Cant find ${req.originalUrl} on this server`);
+    err.status = "fail";
+    err.statusCode = 404;
+    
+    next(err);
+   
+});
+
+app.use((err,req,res,next) => {
+
+   console.log(err.stack);
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';  
+    
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     });
-})
+    
+    next();
+});
 
 module.exports = app;
 
