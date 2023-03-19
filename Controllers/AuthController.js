0fd555a7491ca4e8a40d74a70538fcd30998aaa0
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-19 23:20:20 +0300

    user authentication

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -3,20 +3,23 @@ const User = require('./../Models/User');
 exports.signup = async(req,res) => {
 
 try {
-    const user = await User.create(req.body);
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+    
     res.status(201).json({
         status: 'success',
         data: {
            user
         }
     });
-    
-
 } catch (error) {
     return res.status(400).json({
         status: 'fail',
         message: error
      });
-}
-  
+  }  
 }
