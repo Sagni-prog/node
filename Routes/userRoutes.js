909399b3339c5d16b37784e1c1623af1f3547361
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-23 10:55:33 +0300

    user route refactoring

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,10 +3,14 @@ const AuthController = require('./../Controllers/AuthController');
 
 const router = express.Router();
 
-router.get('/',AuthController.protect,AuthController.authorize('admin'),AuthController.index);
-router.post('/signup',AuthController.signup);
-router.post('/login',AuthController.login);
-// router.post('/forgetpassword',AuthController.forgetPassword);
+    router.get('/',AuthController.protect,AuthController.authorize('admin'),AuthController.index);
+    router.post('/signup',AuthController.signup);
+    router.post('/login',AuthController.login);
+    router.post('/forget-password',AuthController.forgetPassword);
+    router.patch('/reset-password/:token',AuthController.resetPassword);
+    router.patch('/update-password',AuthController.protect,AuthController.updatePassword); router.patch('/update',AuthController.protect,AuthController.udateUser);
+    router.delete('/delete',AuthController.protect,AuthController.deleteUser);
+
 
 
 module.exports = router;
