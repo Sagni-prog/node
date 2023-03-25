96738ab584f3e3c8a084402705abf648c14308b1
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-03-25 16:44:21 +0300

    like

--- Controllers/LikeController.js
+++ Controllers/LikeController.js
@@ -2,39 +2,69 @@ const Like = require('./../Models/Like');
 const Post = require('./../Models/Post');
 const AppError = require('./../utils/appError');
 
+const likeIt = async (id) =>{
+         const like = await Like.findById(id);
+            like.isLiked = !like.isLiked;
+           await like.save();
+}
+
+const createLike = async (post,user) => {
+    const like = await Like.create({
+        isLiked: true,
+        liker: user,
+        post: post
+   });
+   
+   post.likes[post.likes.length] = like._id;
+   await post.save();
+    return true;
+}
+
 exports.postLike = async (req,res,next) => {
     try {
-        const post = await Post.findById(req.params.postId).populate('comments');
+        const post = await Post.findById(req.params.postId).populate('likes');
+        
         if(!post){
            return next(
                  new AppError('post with this Id does not exist',404)
            );
         }
         
-        if(post.likes.length) {
-           const like = await Like.create({
-                isLiked: true,
-                liker: req.user,
-                post: post
-           });
-           
-           post.likes[post.likes.length] = like._id;
-           post.save();
+        if(!post.likes.length) {
+         
+            await createLike(post,req.user);
         }
-      
-    
-        post.likes.map((like) => {
-            if(like.author._id.equals(req.user._id)){
-                like.isLiked = !like.isLiked;
-                post.save();
+        
+  
+      post.likes.map((like) => {
+         let index = 0; 
+         index = index + 1;
+            if(like.liker._id.equals(req.user._id)){
+            
+                likeIt(like._id);
+                
+            } 
+            if(post.likes.length === index + 1){
+                createLike(post,req.user);
+                
+                res.json({
+                   index: index,
+                   len: post.likes.length,
+                   data: {
+                   post
+                   }
+                })
+                // console.log("end of the array")
             }
+            
+            res.status(201).json({
+               status: 'sucess',
+               data: {
+               post
+               }
+            })
         });
     
-        
-        // if(post.likes.author._id.equals(req.user._id)){
-        
-        // }
-        
     } catch (error) {
         next(error)
     }
